c09be5499cf0ede69f494c12d63e63a72c1b99e1
javiermartins <javiimartinserrano@gmail.com>
2024-10-26 19:51:37 +0200

    Changing empty urls message and animate-fade-out in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,15 @@ module.exports = {
   theme: {
     extend: {
       animation: {
+        'fade-out': 'fadeOut 0.6s forwards',
         'fade-in-left': 'fade-in-left 0.5s ease-in-out forwards',
         'fade-in-bottom': 'fade-in-bottom 0.5s ease-in-out forwards',
       },
       keyframes: {
+        fadeOut: {
+          '0%': { opacity: '1', transform: 'scale(1)' },
+          '100%': { opacity: '0', transform: 'scale(0)' },
+        },
         'fade-in-left': {
           '0%': { opacity: '0', transform: 'translateX(-20px)' },
           '100%': { opacity: '1', transform: 'translateX(0)' },
