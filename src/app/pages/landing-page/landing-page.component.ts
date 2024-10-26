b1517fc855f96243c56b6ca16f52e0876a5466d7
javiermartins <javiimartinserrano@gmail.com>
2024-10-26 16:05:01 +0200

    Landing page

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, TranslateModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
 
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
+
+  async shortUrl() {
+    const isAuth = await this.authService.isAuthenticated();
+
+    if (isAuth) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-in-left': 'fade-in-left 0.5s ease-in-out forwards',
+        'fade-in-bottom': 'fade-in-bottom 0.5s ease-in-out forwards',
+      },
+      keyframes: {
+        'fade-in-left': {
+          '0%': { opacity: '0', transform: 'translateX(-20px)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        'fade-in-bottom': {
+          '0%': { opacity: '0', transform: 'translateY(-20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+    },
   },
-  plugins: [],
-}
-
+};
