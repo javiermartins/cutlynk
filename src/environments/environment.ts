356e6a745606314c3aee318ddefb0ef07c358a8e
javiermartins <javiimartinserrano@gmail.com>
2024-11-20 21:07:16 +0100

    Environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,12 @@
 export const environment = {
+
+    BASE_URL: '',
+
     API_ENDPOINT: '',
     PROJECT_ID: '',
     DATABASE_ID: '',
 
     USERS_COLLETION: '',
     URL_COLLETION: '',
+    CATEGORY_COLLETION: '',
 };
