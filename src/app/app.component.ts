c5cc4cbd9d12304b1c6b4326d333324b7dc1d86c
javiermartins <javiimartinserrano@gmail.com>
2025-05-02 18:18:39 +0200

    Add meta descriptions dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { LangChangeEvent, TranslateService } from "@ngx-translate/core";
 import { TuiRoot } from "@taiga-ui/core";
 import languages from "./data/languages";
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -15,15 +16,22 @@ export class AppComponent implements OnInit {
 
   constructor(
     private translateService: TranslateService,
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private meta: Meta
   ) { }
 
   async ngOnInit() {
     await this.setLanguage();
 
-    this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
-      this.renderer.setAttribute(document.documentElement, 'lang', event.lang);
-    });
+  const initialDescription = this.translateService.instant('META.DESCRIPTION');
+  this.meta.updateTag({ name: 'description', content: initialDescription });
+
+  this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+    this.renderer.setAttribute(document.documentElement, 'lang', event.lang);
+
+    const description = this.translateService.instant('META.DESCRIPTION');
+    this.meta.updateTag({ name: 'description', content: description });
+  });
   }
 
   async setLanguage() {
